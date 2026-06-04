a76d695ac657d666b4b202f2507667623f63e5bd
tetodevaseel-bit <tetodevaseel@gmail.com>
2026-06-04 04:43:49 +0300

    Clean vite.config: remove unused polyfills

--- vite.config.js
+++ vite.config.js
@@ -3,19 +3,5 @@ import vue from '@vitejs/plugin-vue';
 
 export default defineConfig({
   plugins: [vue()],
-  build: { outDir: 'dist' },
-  define: {
-    global: 'globalThis',
-    'process.env': {},
-    'process.version': '"v18.0.0"',
-    'process.platform': '"browser"',
-  },
-  resolve: {
-    alias: {
-      buffer: 'buffer',
-    }
-  },
-  optimizeDeps: {
-    include: ['buffer'],
-  }
+  build: { outDir: 'dist' }
 });
